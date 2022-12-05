d32d32840b22c167e425e9c72bc7518b72300bd3
Angel <amunoz@devware.mx>
2022-12-05 10:00:18 -0600

    Git Final 05/12/22

---
+++ SCT_iCare/Controllers/CorteCaja/CorteCajaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using conekta;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SCT_iCare;
+using System.IO;
+using System.Text;
+using System.Globalization;
+using MessagingToolkit.QRCode.Codec;
+using System.Drawing;
+using System.IO.Compression;
+using PagedList;
+
+namespace SCT_iCare.Controllers.CorteCaja
+{
+    public class CorteCajaController : Controller
+    {
+        // GET: CorteCaja
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
